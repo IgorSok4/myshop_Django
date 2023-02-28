1fffd4aa9067ded56ee55adc6dda1768af0dd621
IgorS <sok.igor4@gmail.com>
2023-02-28 21:44:59 +0100

    implemented new app "myshop", updated models, views, urls

---
+++ myshop/shop/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200,
+                            db_index=True)
+    slug = models.SlugField(max_length=200,
+                            unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.IntegerField(blank=True)
+    # weight = models.DecimalField(max_digits=3, decimal_places=1)
+    
+    your_choices = (
+        ('piece', u'piece'),
+        ('kg', u'kg'),
+        ('l', u'l'),
+        ('g1', u'500 g'),
+        ('g2', u'100 g'),
+        ('g3', u'50 g'),
+        ('ml1', u'500 ml'),
+        ('ml2', u'100 ml'),
+        ('ml3', u'50 ml'),
+    )
+    measure = models.CharField(max_length=20, choices=your_choices, null=True, blank=True)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
