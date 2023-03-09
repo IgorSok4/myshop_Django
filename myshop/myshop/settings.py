23c24bde7d27c2b4aea9686aed30f1b5810d44e5
IgorS <sok.igor4@gmail.com>
2023-03-10 00:01:14 +0100

    implemented cart app, sessions

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
+    'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
     
 ]
 
@@ -126,3 +127,5 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
